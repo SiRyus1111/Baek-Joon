6803ad2f66360e091ff714cdba87c9c1b06d7c9c
SiRyus <anniefun1111@gmail.com>
2025-12-06 02:34:10 +0900

    boj 1149 complete
    백준 1149번(RGB거리). 저번엔 못 풀었지만 이번엔 그리 어렵지 않게 풀었다.

---
+++ boj1149.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+dp = [None] * n
+for i in range(n):
+    arr = list(map(int, sys.stdin.readline().split(" ")))
+    dp[i] = arr
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i-1][0],dp[i-1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i-1][1],dp[i-1][0]) + dp[i][2]
+print(min(dp[n-1][0],dp[n-1][1],dp[n-1][2]))
