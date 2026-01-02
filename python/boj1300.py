cd49f32013a25b37638c9839af756493799a5aca
SiRyus1111 <anniefun1111@gmail.com>
2026-01-02 18:55:43 +0900

    boj 1300 complete

--- python/boj1300.py
+++ python/boj1300.py
@@ -1,2 +1,18 @@
 import sys
 input = sys.stdin.readline
+n = int(input())
+k = int(input())
+start = 1
+end = n * n
+result = 0
+while start <= end:
+    mid = (start + end) //2
+    cnt = 0
+    for i in range(1, n+1):
+        cnt += min(n, mid // i)
+    if cnt >= k:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+print(result)
