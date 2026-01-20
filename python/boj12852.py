3dd51e5f1447aad9dab9c81fdbdc840c80b40814
SiRyus1111 <anniefun1111@gmail.com>
2026-01-20 11:48:49 +0900

    BOJ 12852 complete (DP)

---
+++ python/boj12852.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+
+n = int(input())
+dp = [0] * (n+1)
+path = [0] * (n+1)
+dp[1] = 0
+
+for i in range(2, n+1):
+    
+    dp[i] = dp[i-1] + 1
+    path[i] = i - 1
+
+    if i % 2 == 0 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
+        path[i] = i//2
+    
+    if i % 3 == 0 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
+        path[i] = i//3
+
+print(dp[n])
+
+cur = n
+while True:
+    print(cur, end = " ")
+    if cur == 1:
+        break
+    cur = path[cur]
